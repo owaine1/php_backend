d220da962990998e9300ad121819b70f1475dfa8
Owain <owaine1@live.com>
2017-10-18 14:09:04 +0800

    It takes both a weapon, and two people, to commit a murder.

--- backend/manage_db/delete_database.php
+++ backend/manage_db/delete_database.php
@@ -6,10 +6,13 @@ $db = new DbConnect($admin, $pass);
 $db_name = $_GET['db_name'];
 function safety($db_name){
   if ($db_name === in_array($restricted_names)){
+    logger('if is true');
+
     $restricted_names = [bill, bob, mysql, performance_schema, sys, information_schema];
 
     // send message to say delete stopped.
   } else {
+    logger('if is false');
     $sql = "DROP DATABASE $db_name";
     //  go through command and delete
     }
