36723d37dffc77d01d477a1283c5c0f758ad3a76
Owain <owaine1@live.com>
2017-10-18 15:19:19 +0800

    To live is always desirable. 		-- Eleen the Capellan, "Friday's Child", stardate 3498.9

--- backend/common/connection.php
+++ backend/common/connection.php
@@ -6,10 +6,10 @@ require_once('credentials.php');
 class DbConnect
 {
   public $conn;
-  public function __construct($admin, $pass)
+  public function __construct($app_user, $app_pass)
   {
     try {
-      $this->conn = new PDO("mysql:host=127.0.0.1", $admin, $pass);
+      $this->conn = new PDO("mysql:host=127.0.0.1";dbname=films, $app_user, $app_pass);
       // set the PDO error mode to exception
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       return $this->conn;
