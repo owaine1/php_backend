0444c59d1c6af4a689b694b76e0165951b55c45f
Owain <owaine1@live.com>
2017-10-18 10:59:27 +0800

    "Benson, you are so free of the ravages of intelligence" 		-- Time Bandits

--- backend/manage_users/create_user.php
+++ backend/manage_users/create_user.php
@@ -6,7 +6,7 @@ sleep('1'); // mimic a 'real' server, kinda...
 $user = new DbConnect($admin, $pass);
 
 $user_name = $_GET['user_name'];
-$user_pass = $_GET['user_name'];
+$user_pass = $_GET['user_pass'];
 $sql = "CREATE USER $user_name";
 
 $result = $user->conn->query($sql);
