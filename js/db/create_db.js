4bb09fbc60c6d307ff265715b1de646cd66c695e
Owain <owaine1@live.com>
2017-10-18 09:35:58 +0800

    Confidence is the feeling you have before you understand the situation.

--- js/db/create_db.js
+++ js/db/create_db.js
@@ -3,12 +3,12 @@ function create_db(){
   var db = $('#db_name').val();
   var data = {db_name: db};
   $.get('backend/manage_db/create_database.php', data).done(created_db).fail(blow_up);
-  $('#create_db').removeClass('btn-primary').addClass('btn-warning');
+  $('#create_db').removeClass('btn-primary').addClass('btn-warning').text('creating...');
 }
 
 function created_db(){
   console.log('Inside created_db');
-  $('#create_db').removeClass('btn-warning').addClass('btn-success');
+  $('#create_db').removeClass('btn-warning').addClass('btn-success').text('created!');
 
   setup();
 }
