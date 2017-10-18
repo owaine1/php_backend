c7dab7f99ad7a84d1d52ee5c1bb9a1ca6a359839
Owain <owaine1@live.com>
2017-10-18 10:14:22 +0800

    Unfair animal names:
    -- tsetse fly			-- bullhead
    -- booby			-- duck-billed platypus
    -- sapsucker			-- Clarence
    		-- Gary Larson

--- backend/manage_users/delete_user.php
+++ backend/manage_users/delete_user.php
@@ -6,12 +6,12 @@ $user = new DbConnect($admin, $pass);
 $user_name = $_GET['user_name'];
 $sql = "DROP USER $user_name";
 
-$result = $db->conn->query($sql);
+$result = $user->conn->query($sql);
 
 if ($result){
   logger("created user");
   echo "dropped user $user_name";
 }
   else {
-    logger($db->conn->error);
+    logger($user->conn->error);
   }
--- backend/manage_users/show_users.php
+++ backend/manage_users/show_users.php
@@ -1,11 +1,11 @@
 <?php
 require_once('../common/connection.php');
 
-$db = new DbConnect($admin, $pass);
+$user = new DbConnect($admin, $pass);
 
-$sql = 'SHOW USERS';
+$sql = 'SELECT User FROM msql.userSHOW USERS';
 
-$result = $db->conn->query($sql);
+$result = $user->conn->query($sql);
 
 if ($result){
   $output = $result->fetchAll();
@@ -13,5 +13,5 @@ if ($result){
   echo json_encode($output);
 }
   else {
-    logger($db->conn->error);
+    logger($user->conn->error);
   }
