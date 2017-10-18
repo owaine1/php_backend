856f9d87f8f28a03a1f8e793b1e78d37639cf953
Owain <owaine1@live.com>
2017-10-18 09:24:14 +0800

    Love the sea?  I dote upon it -- from the beach.

--- js/databases.js
+++ js/databases.js
@@ -10,27 +10,7 @@ function setup(){
   $.get('backend/manage_db/show_databases.php').done(show_dbs).fail(blow_up);
 }
 
-function show_dbs(data){
-  console.log('Inside show_dbs');
-  data = JSON.parse(data); // turns string of characters into json objects
-  console.log(data);
 
-  for (var counter =0; counter < data.length; counter++){
-    console.log(data[counter].Database);
-    var tr = $('<tr>'); //create empty table row
-    var td = $('<td>'); //make first td
-    td.text(data[counter].Database); // add name of db
-    tr.append(td); // add td to row
-    td = $('<td>'); // make second td to hold delete button
-
-    var btn = $('<button class = "delete" id="' + data[counter].Database + '">delete</button>');
-
-    td.append(btn);
-    tr.append(td);
-    $('.table tbody').append(tr);
-  }
-  $('.delete').click(delete_db);
-}
 function delete_db(){
   console.log('Inside delete_db');
   var db_to_be_deleted =$(this).attr('id');
