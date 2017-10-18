5466c52b12cc292dd00699a4f8664eef185456ee
Owain <owaine1@live.com>
2017-10-18 11:28:09 +0800

    FORTUNE PROVIDES QUESTIONS FOR THE GREAT ANSWERS: #19 A:	To be or not to be. Q:	What is the square root of 4b^2?

--- js/users/create_user.js
+++ js/users/create_user.js
@@ -1,7 +1,12 @@
 function create_user(){
   console.log('Inside create_user');
   var user_name = $('#user_name').val();
+  console.log(user_name);
+
   var pass = ('#user_password').val();
+  console.log(pass);
+
+
   var data = {user_name: user_name, password: pass};
   $.get('backend/manage_users/create_user.php', data).done(created_user).fail(blow_up);
   $('#create_user').removeClass('btn-primary').addClass('btn-warning').text('creating...');
