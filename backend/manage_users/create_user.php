9f18ebdbad5c66fca86e43d876e526232019dfcc
Owain <owaine1@live.com>
2017-10-18 10:37:00 +0800

    The cost of living has just gone up another dollar a quart. 		-- W.C. Fields

--- backend/manage_users/create_user.php
+++ backend/manage_users/create_user.php
@@ -6,6 +6,7 @@ sleep('1'); // mimic a 'real' server, kinda...
 $user = new UserConnect($admin, $pass);
 
 $user_name = $_GET['user_name'];
+$user_pass = $_GET['user_name'];
 $sql = "CREATE USER $user_name";
 
 $result = $user->conn->query($sql);
