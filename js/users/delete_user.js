7fe2f6090b8a2c95a10e6dfb5e384616a1f53e68
Owain <owaine1@live.com>
2017-10-18 11:11:07 +0800

    Superior ability breeds superior ambition. 		-- Spock, "Space Seed", stardate 3141.9

--- js/users/delete_user.js
+++ js/users/delete_user.js
@@ -3,7 +3,7 @@ function delete_user(){
   var user_to_be_deleted =$(this).attr('id');
   console.log(user_to_be_deleted);
   var data = {user_name: user_to_be_deleted};
-  $.get('backend/manage_db/delete_user.php', data).done(deleted_user).fail(blow_up);
+  $.get('backend/manage_users/delete_user.php', data).done(deleted_user).fail(blow_up);
 }
 
 function deleted_user(data){
