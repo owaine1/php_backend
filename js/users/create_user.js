1030c501e65b5147e2ff010fdaab3d32aebff417
Owain <owaine1@live.com>
2017-10-18 09:49:49 +0800

    Love America -- or give it back.

--- js/users/create_user.js
+++ js/users/create_user.js
@@ -1,13 +1,14 @@
-function create_db(){
-  console.log('Inside create_db');
-  var db = $('#db_name').val();
-  var data = {db_name: db};
-  $.get('backend/manage_db/create_database.php', data).done(created_db).fail(blow_up);
-  $('#create_db').removeClass('btn-primary').addClass('btn-warning').text('creating...');
+function create_user(){
+  console.log('Inside create_user');
+  var db = $('#user_name').val();
+  var pass = ('#password').val();
+  var data = {user_name: db, password: pass};
+  $.get('backend/manage_db/create_user.php', data).done(created_user).fail(blow_up);
+  $('#create_user').removeClass('btn-primary').addClass('btn-warning').text('creating...');
 }
 
-function created_db(){
-  console.log('Inside created_db');
-  $('#create_db').removeClass('btn-warning').addClass('btn-success').text('created!');
+function created_user(){
+  console.log('Inside created_user');
+  $('#create_user').removeClass('btn-warning').addClass('btn-success').text('created!');
   setup();
 }
