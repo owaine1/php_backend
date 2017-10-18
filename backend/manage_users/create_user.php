1accb542fb9b4411e98763445c87b9ee3bc03a68
Owain <owaine1@live.com>
2017-10-18 10:15:26 +0800

    Superstition, idolatry, and hypocrisy have ample wages, but truth goes a-begging. 		-- Martin Luther

--- backend/manage_users/create_user.php
+++ backend/manage_users/create_user.php
@@ -3,17 +3,17 @@ require_once('../common/connection.php');
 
 sleep('1'); // mimic a 'real' server, kinda...
 
-$db = new DbConnect($admin, $pass);
+$user = new DbConnect($admin, $pass);
 
 $user_name = $_GET['user_name'];
 $sql = "CREATE USER $user_name";
 
-$result = $db->conn->query($sql);
+$result = $user->conn->query($sql);
 
 if ($result){
   logger("created user");
   echo "created user $user_name";
 }
   else {
-    logger($db->conn->error);
+    logger($user->conn->error);
   }
