9210e0646cff2a16618ca9576e77f1b1c8e1890d
Owain <owaine1@live.com>
2017-10-18 14:42:08 +0800

    In 1914, the first crossword puzzle was printed in a newspaper.  The creator received $4000 down ... and $3000 across.

--- backend/manage_users/delete_user.php
+++ backend/manage_users/delete_user.php
@@ -4,14 +4,23 @@ require_once('../common/connection.php');
 $user = new DbConnect($admin, $pass);
 
 $user_name = $_GET['user_name'];
+function safety_db($user_name){
+  $restricted_names = ['bill', 'bob', 'betty', 'root'];
+  if (in_array($user_name, $restricted_names)){
+    logger('if is true');
+    echo "$user_name cannot be deleted!";
+  } else {
+    logger('if is false');
+    global $user;
 $sql = "DROP USER $user_name";
-
 $result = $user->conn->query($sql);
 
 if ($result){
-  logger("created user");
+  logger("deleted user");
   echo "dropped user $user_name";
 }
   else {
     logger($user->conn->error);
   }
+}
+safety_db($user_name);
