0045f27f11c0a6e3e2932616f813d8149d30c5a8
Owain <owaine1@live.com>
2017-10-18 11:24:56 +0800

    He only knew his iron spine held up the sky -- he didn't realize his brain had fallen to the ground. 		-- The Book of Serenity

--- js/users/show_users.js
+++ js/users/show_users.js
@@ -4,14 +4,14 @@ function show_user(data){
   console.log(data);
 
   for (var counter =0; counter < data.length; counter++){
-    console.log(data[counter].Database);
+    console.log(data[counter].User);
     var tr = $('<tr>'); //create empty table row
     var td = $('<td>'); //make first td
-    td.text(data[counter].Database); // add name of db
+    td.text(data[counter].User); // add name of db
     tr.append(td); // add td to row
     td = $('<td>'); // make second td to hold delete button
 
-    var btn = $('<button class = "delete" id="' + data[counter].Database + '">delete</button>');
+    var btn = $('<button class = "delete" id="' + data[counter].User + '">delete</button>');
 
     td.append(btn);
     tr.append(td);
