8c0ae3723adf60b17c462c0c0c70695d003e4592
Owain <owaine1@live.com>
2017-10-18 10:00:31 +0800

    In a great romance, each person basically plays a part that the other really likes. 		-- Elizabeth Ashley

--- backend/manage_users/create_user.php
+++ backend/manage_users/create_user.php
@@ -6,13 +6,13 @@ sleep('1'); // mimic a 'real' server, kinda...
 $db = new DbConnect($admin, $pass);
 
 $db_name = $_GET['db_name'];
-$sql = "CREATE DATABASE $db_name";
+$sql = "CREATE USER $db_name";
 
 $result = $db->conn->query($sql);
 
 if ($result){
-  logger("created database");
-  echo "created database $db_name";
+  logger("created user");
+  echo "created user $db_name";
 }
   else {
     logger($db->conn->error);
--- backend/manage_users/delete_user.php
+++ backend/manage_users/delete_user.php
@@ -4,13 +4,13 @@ require_once('../common/connection.php');
 $db = new DbConnect($admin, $pass);
 
 $db_name = $_GET['db_name'];
-$sql = "DROP DATABASE $db_name";
+$sql = "DROP USER $db_name";
 
 $result = $db->conn->query($sql);
 
 if ($result){
-  logger("created database");
-  echo "dropped database $db_name";
+  logger("created user");
+  echo "dropped user $db_name";
 }
   else {
     logger($db->conn->error);
