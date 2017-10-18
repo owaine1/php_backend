016b15c2aec77cd515b2586569a95e7f884a8169
Owain <owaine1@live.com>
2017-10-18 10:03:44 +0800

    You might have mail.

--- backend/manage_users/delete_user.php
+++ backend/manage_users/delete_user.php
@@ -4,13 +4,13 @@ require_once('../common/connection.php');
 $db = new DbConnect($admin, $pass);
 
 $db_name = $_GET['db_name'];
-$sql = "DROP USER $db_name";
+$sql = "DROP USER $user_name";
 
 $result = $db->conn->query($sql);
 
 if ($result){
   logger("created user");
-  echo "dropped user $db_name";
+  echo "dropped user $user_name";
 }
   else {
     logger($db->conn->error);
