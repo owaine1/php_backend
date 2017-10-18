8eb56632251988bc3f3225a6884dfa0be5b4b49f
Owain <owaine1@live.com>
2017-10-18 10:02:39 +0800

    Pick another fortune cookie.

--- backend/manage_users/create_user.php
+++ backend/manage_users/create_user.php
@@ -5,14 +5,14 @@ sleep('1'); // mimic a 'real' server, kinda...
 
 $db = new DbConnect($admin, $pass);
 
-$db_name = $_GET['db_name'];
-$sql = "CREATE USER $db_name";
+$user_name = $_GET['user_name'];
+$sql = "CREATE USER $user_name";
 
 $result = $db->conn->query($sql);
 
 if ($result){
   logger("created user");
-  echo "created user $db_name";
+  echo "created user $user_name";
 }
   else {
     logger($db->conn->error);
