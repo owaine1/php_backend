7744d5ceb7eb0828040298eeedd309296a8e2a17
Owain <owaine1@live.com>
2017-10-20 13:39:42 +0800

    "...a most excellent barbarian ... Genghis Kahn!" -- _Bill And Ted's Excellent Adventure_

--- backend/admin/login.php
+++ backend/admin/login.php
@@ -11,8 +11,12 @@ $user_name = $_GET['user'];
 $user_pass = md5($_GET['pass']);
 logger($user_name . $user_pass);
 
-$sql = "SELECT * FROM users WHERE user = '$user_name' && password = '$user_pass'";
-$result = $db->conn->query($sql);
+// $sql = "SELECT * FROM users WHERE user = '$user_name' && password = '$user_pass'";
+// $result = $db->conn->query($sql);
+$STMT = $db->conn->prepare("SELECT * FROM users WHERE user = :user_name && pass = :user_pass");
+$result->bindParam(':user_name', $user_name);
+$result->bindParam(':user_pass', $user_pass);
+$result->execute();
 
 if ($result->rowCount() == 1) {
   logger("user is registered");
