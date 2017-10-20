c45cd9241d6ca9685d48d901e0f47f768f66ba94
Owain <owaine1@live.com>
2017-10-20 10:25:10 +0800

    This fortune intentionally says nothing.

---
+++ backend/admin/logout.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require_once('../common/logger.php');
+
+sleep('1'); // mimic a 'real' server, kinda...
+
+$_SESSION['logged_in'] = false;  //users th $_SESSION global array
+echo 'logged out...'; //send message to js front-end
--- js/javascript.js
+++ js/javascript.js
@@ -18,10 +18,30 @@ function do_login(){
     user: user,
     pass: pass
   };
+$.get('backend/admin/login.php', signin_creds).done(logged_in);
+}
+function logged_in(data){
+  console.log('Inside logged_in');
+  console.log(data);
+  if(data == 'logged in'){
+    $('#outside').slipdeUp(800);
+    $('#inside').slipdeDown(800);
+  }
+  else{
+    $('message').text('Sorry you must be logged in');
+  }
 
 }
 function do_logout(){
-  console.log('Inside do_logout');
+  console.log('Inside logged_out');
+  $.get('backend/admin.logout.php').done(logged_out).fail(blow_up);
+}
+
+function logged_out(){
+  console.log('Inside logged_out');
+  console.log(data);
+  window.location = 'index.php'; // redirect to homepage
+
 }
 
 function check_backend(){
